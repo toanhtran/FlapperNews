7b580af4aada226def3f964644044c1dda45f73e
toanhtran <toanh_tran@yahoo.com>
2015-11-19 16:09:32 +0900

    add home route

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.factory('posts', [function(){
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
+app.config([
+	'stateProvider',
+	'urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+
+		$stateProvider
+		.state('home', {
+			url:'/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+	$urlRouterProvider.otherwise('home');
+}]);
 
 app.controller('MainCtrl',[
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
 	$scope.test = 'Hello world!';
-$scope.posts = [
-	{title: 'post 1', upvotes: 5},
-	{title: 'post 2', upvotes: 2},
-	{title: 'post 3', upvotes: 15},
-	{title: 'post 4', upvotes: 9},
-	{title: 'post 5', upvotes: 4}
-];
+$scope.posts = post.posts;
+// [
+// 	{title: 'post 1', upvotes: 5},
+// 	{title: 'post 2', upvotes: 2},
+// 	{title: 'post 3', upvotes: 15},
+// 	{title: 'post 4', upvotes: 9},
+// 	{title: 'post 5', upvotes: 4}
+// ];
 $scope.addPost = function() {
 	if(!scope.title || $scope.title == '') {return; }
 	$scope.post.push({
